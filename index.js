4451911adc83081423f603580ddcf5560ca0077a
Yu Li <yli0607x@gmail.com>
2018-09-20 18:16:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ function nowServing(katzDeliLine){
     retun (`We are currently ${katzDeliLine.shift()}.`)
   }
 }
+
+function currentLine(line){
+  if (katzDeliLine.length==0){
+    return ("The line is currently empty.")
+  }
+  const array =[]
+  for (let x = 0; x< line.length; x++)
+  array.push (`${x+1}. ${line[x]}`)
+  return `The line is currently: ${array.join(", ")}`
+}
