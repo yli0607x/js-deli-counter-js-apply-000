ec6b63b9de20b02a058da66769faa28b7864856f
Yu Li <yli0607x@gmail.com>
2018-09-20 18:12:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function takeANumber(line, name){
 line.push(name)
 return (`Welcome, ${name}. You are number ${line.length} in line.`)
 }
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length==0){
+    return ("There is nobody waiting to be served!")
+  }
+  else {
+    retun (`We are currently ${katzDeliLine.shift()}.`)
+  }
+}
